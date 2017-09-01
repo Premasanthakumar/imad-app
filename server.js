55072dbb9a97a9998c088a373f0376c6929f33c2
Premasanthakumar <prema17kumar@gmail.com>
2017-09-01 22:57:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,16 +80,13 @@ app.get('/:articleName', function (req, res) {
    res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article_two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article_two.html'));
-});
-
-app.get('/article_three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article_three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
