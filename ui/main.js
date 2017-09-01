3e6cedad51e3b6243e97335793bfaa14449db578
Premasanthakumar <prema17kumar@gmail.com>
2017-09-01 23:21:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHtml = "newValue";
 //move image
-var image = document.getElementById("modi");
+var img = document.getElementById("modi");
 img.onClick = function ()
 {
     img.style.marginLeft = '100px';
