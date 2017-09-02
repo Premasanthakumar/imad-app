e4f64d3fbf5f2aef1bdc8d0ac7f2ba6f8e6732f5
Premasanthakumar <prema17kumar@gmail.com>
2017-09-02 20:06:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,10 @@ function createTemplate(data)
 }
 
 
+app.get('/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -89,9 +93,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/favicon.ico', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
